59a32727d4fd2ebd6a35ec500118fd52568c5024
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-07-20 20:32:39 +0100

    green: should return the word when column number is higher than the word

--- test/query_wrap_spec.rb
+++ test/query_wrap_spec.rb
@@ -3,12 +3,10 @@ require 'rspec'
   #TODO LIST
   # A static class named Wrap or Wrapper that contains a function that format a query
   # The function has to split a text like a word processor does
-  # - should return the word when column number is higher than the word
   # - should return the word split when the column is lower than the word
   # - should return the sentence split by spaces when the column is lower than the sentence
   # - should return the sentence split in the last space when two words fits in the column size
   # EXAMPLES:
-  # ("hello", 5) -> "hello"
   # ("hello", 3) -> "hel\nlo"
   # ("hello how are you", 5) -> "hello\nhow\nare\nyou"
   # ("hello is this", 9) -> "hello is\nthis"
@@ -25,5 +23,9 @@ describe 'Wrapper' do
 
   it 'should return the word when column is zero' do
     expect(wrap('word', 0)).to(eq('word'))
+  end
+
+  it 'should return the word when column number is higher than the word' do
+    expect(wrap('word', 5)).to(eq('word'))
   end
 end
