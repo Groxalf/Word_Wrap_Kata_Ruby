f32ed1f2add0c9ab6fdbb1d5b580df00db68e230
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-07-20 20:28:35 +0100

    ToDo List: added thins to do matching the examples

--- test/query_wrap_spec.rb
+++ test/query_wrap_spec.rb
@@ -3,6 +3,12 @@ require 'rspec'
   #TODO LIST
   # A static class named Wrap or Wrapper that contains a function that format a query
   # The function has to split a text like a word processor does
+  # - should return empty string when an empty string is the query input
+  # - should return the word when column is zero
+  # - should return the word when column number is higher than the word
+  # - should return the word split when the column is lower than the word
+  # - should return the sentence split by spaces when the column is lower than the sentence
+  # - should return the sentence split in the last space when two words fits in the column size
   # EXAMPLES:
   # ("", 0) -> ""
   # ("hello", 0) -> "hello"
@@ -13,6 +19,5 @@ require 'rspec'
 
 
 def wrap(query, column)
-  ''
-end
 
+end
