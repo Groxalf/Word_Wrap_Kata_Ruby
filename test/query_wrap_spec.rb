68d73461f4226e9f240bd5fb8de17c1b2c74c0e4
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-07-20 20:20:36 +0100

    definition: kata problem

---
+++ test/query_wrap_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+
+  #TODO LIST
+  # A static class named Wrap or Wrapper that contains a function that format a query
+  # The function has to split a text like a word processor does
+  # EXAMPLES:
+  # ("", 0) -> ""
+  # ("hello", 0) -> "hello"
+  # ("hello", 5) -> "hello"
+  # ("hello", 3) -> "hel\nlo"
+  # ("hello how are you", 5) -> "hello\nhow\nare\nyou"
+  # ("hello is this", 9) -> "hello is\nthis"
+
+
+def wrap(query, column)
+  ''
+end
+
