100f575ee25609dd54014fd8ab4cede5b3d2dd41
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2015-07-25 00:09:44 +0100

    refactor: change variable name

--- wrapper.rb
+++ wrapper.rb
@@ -3,7 +3,7 @@ class Wrapper
   def self.wrap(query, column)
     return query if column == 0
     if query.size > column
-      return no_space_query_divider(column, query) if get_cut_position(column, query).eql? nil
+      return no_space_query_divider(column, query) if get_cut_position(column, query).equal? nil
       return middle_space_query_divider(column, get_cut_position(column, query), query)
     end
     query
@@ -11,14 +11,14 @@ class Wrapper
 
   def self.middle_space_query_divider(column, cut_position, query)
     first_word = query[0, cut_position]
-    second_word = wrap(query[cut_position + 1, query.size - 1], column)
-    return first_word + "\n" + second_word
+    remaining_query = wrap(query[cut_position + 1, query.size - 1], column)
+    return first_word + "\n" + remaining_query
   end
 
   def self.no_space_query_divider(column, query)
     first_word = query[0, column]
-    second_word = wrap(query[column, query.size - 1], column)
-    return first_word + "\n" + second_word
+    remaining_query = wrap(query[column, query.size - 1], column)
+    return first_word + "\n" + remaining_query
   end
 
   def self.get_cut_position(column, query)
